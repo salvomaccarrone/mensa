29320d4a403b8967763156f36b3d4d1bb4392715
Federico <xmey95@gmail.com>
2021-03-29 22:30:13 +0200

    Implemented my reservation card inside index.php file (HomePage)

--- index.php
+++ index.php
@@ -4,10 +4,93 @@ session_start();
 if (isset($_SESSION['session_id'])) {
     $session_user = htmlspecialchars($_SESSION['session_user'], ENT_QUOTES, 'UTF-8');
     $session_id = htmlspecialchars($_SESSION['session_id']);
-    
-    printf("Benvenuto %s, il tuo session ID è %s", $session_user, $session_id);
-    echo "<br>";
-    printf("%s", '<a href="./php/logout.php">logout</a>');
 } else {
     header('Location: ./login.html');
 }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CanteenForYou</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <a class="navbar-brand" href="#">CanteenForYou</a>
+        </div>
+    </nav>
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row text-center mt-2">
+            <div class="col-xs-12">
+                <div class="card">
+                    <h5 class="card-header">La tua prenotazione</h5>
+                    <div class="card-body">
+                        <div class="card-group mb-2">
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">Card title</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                            <button class="btn btn-primary" type="button">
+                                <i class="bi bi-pencil-fill"></i>
+                            </button>
+                            <button class="btn btn-primary" type="button">
+                                <i class="bi bi-trash-fill"></i>
+                            </button>
+                            <button class="btn btn-primary" type="button">
+                                <i class="bi bi-upc-scan"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row text-center mt-2">
+            <div class="col-lg-3 col-xs-12">
+                
+            </div>
+            <div class="col-lg-9 col-xs-12">
+                
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>

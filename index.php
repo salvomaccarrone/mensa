d52e7ab02c7080ad1c7bc87e730b7bd746d91c3f
Federico <xmey95@gmail.com>
2021-03-29 22:55:11 +0200

    Inserted modals inside index.php (HomePage)

--- index.php
+++ index.php
@@ -28,6 +28,11 @@ if (isset($_SESSION['session_id'])) {
 </head>
 
 <body>
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+        crossorigin="anonymous"></script>
+
     <!-- Navigation -->
     <nav class="navbar navbar-dark bg-primary">
         <div class="container">
@@ -66,10 +71,10 @@ if (isset($_SESSION['session_id'])) {
                             <button class="btn btn-primary" type="button">
                                 <i class="bi bi-pencil-fill"></i>
                             </button>
-                            <button class="btn btn-primary" type="button">
+                            <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#eliminazionepren">
                                 <i class="bi bi-trash-fill"></i>
                             </button>
-                            <button class="btn btn-primary" type="button">
+                            <button class="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#qrcodepanel">
                                 <i class="bi bi-upc-scan"></i>
                             </button>
                         </div>
@@ -87,10 +92,39 @@ if (isset($_SESSION['session_id'])) {
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-        crossorigin="anonymous"></script>
+    <!-- Codice piatto -->
+    <div class="modal fade" id="qrcodepanel" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Codice piatto</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Conferma eliminazione -->
+    <div class="modal" tabindex="-1" id="eliminazionepren">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Eliminazione prenotazione</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Sei sicuro di voler eliminare la prenotazione?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Si, procedi</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
